796f7c68d521bab999e5f75061ee9e52d6fa09b1
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-10-13 20:56:00 -0300

    vectors and array and control structureimplement section experience

--- 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
+++ 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
@@ -6,22 +6,92 @@ fullStackPHPClassName("02.06 - Arrays, vetores e pilhas");
  * [ arrays ] https://php.net/manual/pt_BR/language.types.array.php
  */
 fullStackPHPClassSession("index array", __LINE__);
+$arrA = array(1, 2, 3);
+$arrA = [0, 1, 2, 3];
 
+var_dump($arrA);
 
+$arrayIndex =[ 
+  "Brian", 
+  "Douglas", 
+  "Jorge",
+];
+
+$arrayIndex[] = "Joaquim";
+$arrayIndex[] = "Philiph";
+
+var_dump($arrayIndex);
 /**
  * [ associative array ] "key" => "value"
  */
 fullStackPHPClassSession("associative array", __LINE__);
+
+$arrayAssociative = [
+  "vocal" => "Brian",
+  "solo_guitar" => "Douglas",
+  "base_bass" => "Jorge",
+  "base_guitar" => "Cliff"
+];
+
+$arrayAssociative["drums"] = "Joaquim";
+$arrayAssociative["rock_band"] = "AC/DC";
+
+var_dump($arrayAssociative);
 
 
 /**
  * [ multidimensional array ] "key" => ["key" => "value"]
  */
 fullStackPHPClassSession("multidimensional array", __LINE__);
+$brian = ["Brian", "Mic"];
+$angus = ["name" => "Douglas", "instrument" => "Guitar"];
+$instrument = [
+  $brian,
+  $angus,
+];
 
+var_dump($instrument);
+
+var_dump([
+  "brian" => $brian,
+  "angus" => $angus
+]);
 
 /**
  * [ array access ] foreach(array as item) || foreach(array as key => value)
  */
 fullStackPHPClassSession("array access", __LINE__);
+$acdc = [
+  "vocal" => "Brian",
+  "solo_guitar" => "Douglas",
+  "base_bass" => "Jorge",
+  "base_guitar" => "Cliff",
+  "drums" => "Joaquim",
+];
 
+echo "<p>{$acdc["vocal"]}</p>";
+echo "<p>{$acdc["solo_guitar"]}</p>";
+echo "<p>{$acdc["base_bass"]}</p>";
+echo "<p>{$acdc["base_guitar"]}</p>";
+echo "<p>{$acdc["drums"]}</p>";
+
+$pearl = [
+  "vocal" => "Phil",
+  "solo_guitar" => "Mkey",
+  "base_bass" => "Stone",
+  "base_guitar" => "Jef",
+  "drums" => "Jack",
+];
+
+$rock_bands = [
+  "AC/DC" => $acdc,
+  "Pearl Jam" => $pearl
+];
+
+var_dump($rock_bands);
+
+echo "<p>{$rock_bands["AC/DC"]["vocal"]}</p>";
+
+foreach ($acdc as $item) {
+  echo "<p>{$item}</p>";
+}
--- 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
+++ 02-iniciando-do-zero-com-php/02-07-estruturas-de-controle/index.php
@@ -1,14 +1,53 @@
 <?php
 require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("02.07 - Estruturas de controle");
+$test = true;
 
+if($test) {
+  var_dump(true);
+} else {
+  var_dump(false);
+}
+
+$age = 51;
+if($age < 18) {
+  var_dump( "bandas colorias");
+} elseif($age > 20 && $age < 50) {
+  var_dump( "ótimas bandas");
+}
+else {
+  var_dump( "rock and roll");
+}
+
+$hour = 3;
+if($hour < 12) {
+  var_dump("bom dia");
+} elseif($hour < 18) {
+  var_dump("boa tarde");
+} else {
+  var_dump("boa noite");
+}
 /*
  * [ if ] https://php.net/manual/pt_BR/control-structures.if.php
  * [ elseif ] https://php.net/manual/pt_BR/control-structures.elseif.php
  * [ else ] https://php.net/manual/pt_BR/control-structures.else.php
  */
 fullStackPHPClassSession("if, elseif, else", __LINE__);
+$rock ="";
 
+if(isset($rock)) {
+  var_dump("rock and roll");
+} else {
+  var_dump("die");
+}
+
+$rockAndRoll = "nirvana";
+
+if(!empty($rockAndRoll)) {
+  var_dump("esta tocando {$rockAndRoll}");
+} else {
+  var_dump("não esta tocando nada");
+}
 
 /*
  * [ isset ] https://php.net/manual/pt_BR/function.isset.php
@@ -22,6 +61,25 @@ fullStackPHPClassSession("isset, empty, !", __LINE__);
  */
 fullStackPHPClassSession("switch", __LINE__);
 
+$payment = "past_due";
 
-
+switch($payment) {
+  case "billet_printed":
+    var_dump("boleto impresso");
+    break;
+  case "cancelled":
+    var_dump("cancelado");
+    break;
+  case "past_due":
+  case "pending":
+    var_dump("aguardando pagamento");
+    break;
+  case "approved":
+  case "completed":
+    var_dump("pagamento aprovado");
+    break;
+  default:
+    var_dump("nao identificado");
+    break;
+}
 
