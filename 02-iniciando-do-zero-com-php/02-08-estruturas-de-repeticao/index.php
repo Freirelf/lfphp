178d3e042f1cd063de36873d8730f30b15614648
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-10-14 18:54:43 -0300

    reproduction structures | closures and generators

--- 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
+++ 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
@@ -7,22 +7,65 @@ fullStackPHPClassName("02.08 - Estruturas de repetição");
  * [ do while ] https://php.net/manual/pt_BR/control-structures.do.while.php
  */
 fullStackPHPClassSession("while, do while", __LINE__);
+$loop = 1;
+$while = [];
 
+while($loop <= 10) {
+  $while[] = $loop;
+  $loop++;
+}
+
+var_dump($while);
+
+$loop = 10;
+$while = [];
+
+do {
+  $while[] = $loop;
+  $loop--;
+} while($loop >= 1);
+var_dump($while);
 
 /*
  * [ for ] https://php.net/manual/pt_BR/control-structures.for.php
  */
 fullStackPHPClassSession("for", __LINE__);
-
+for ($i = 1; $i <= 10; $i++) {
+  echo "<p>{$i}</p>";
+}
 
 /**
  * [ break ] https://php.net/manual/pt_BR/control-structures.break.php
  * [ continue ] https://php.net/manual/pt_BR/control-structures.continue.php
  */
 fullStackPHPClassSession("break, continue", __LINE__);
+for($c = 1; $c <= 10; $c++) {
+  if($c %2 == 1) {
+    continue;
+  }
 
+  if($c >= 10){
+    break;
+  }
+
+  echo "<p>Pulou + 2 :: {$c}</p>";
+}
 
 /**
  * [ foreach ] https://php.net/manual/pt_BR/control-structures.foreach.php
  */
 fullStackPHPClassSession("foreach", __LINE__);
+
+$array = [];
+for($ar = 0; $ar <= 2; $ar++) {
+  $array[] = $ar;
+}
+var_dump($array);
+
+foreach($array as $value) {
+  var_dump($value);
+}
+
+foreach($array as $key => $value) {
+  var_dump("$key = $value");
+}
--- 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
+++ 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
@@ -6,9 +6,65 @@ fullStackPHPClassName("02.09 - Closures e generators");
  * [ closures ] https://php.net/manual/pt_BR/functions.anonymous.php
  */
 fullStackPHPClassSession("closures", __LINE__);
+$myAge = function($year) {
+  $age = date("Y") - $year;
+  return "<h5> Você tem $age anos</h5>";
+};
 
+echo $myAge(2000);
+
+$priceBrl = function($price) {
+  return number_format($price, 2, ",", ".");
+};
+
+echo "<p>O preço do projeto custa R$ {$priceBrl(1000)}</p>";
+
+$myCart =[];
+$myCart["totalPrice"] = 0;
+
+$cardAdd = function($item, $qtd, $price) use (&$myCart) {
+  $myCart[$item] = $qtd * $price;
+  $myCart["totalPrice"] += $myCart[$item];
+};
+
+$cardAdd("notebook", 1, 5000);
+$cardAdd("smartphone", 1, 10000);
+$cardAdd("mouse", 1, 100);
+$cardAdd("monitor", 1, 1500);
+
+var_dump($myCart);
+
+echo "<p>O preço total da compra é R$ {$priceBrl($myCart["totalPrice"])}</p>";
 
 /*
  * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
  */
 fullStackPHPClassSession("generators", __LINE__);
+
+$iterator = 400;
+
+function showDates($days) {
+  $saveDate = [];
+  for($day = 1; $day < $days; $day++) {
+    $saveDate[] = date("d/m/Y", strtotime("+$day days"));
+  }
+  return $saveDate;
+}
+
+echo "<div style='text-align: center'>";
+foreach(showDates(0) as $date) {
+  echo "<small class='tag'>{$date}</small>" . PHP_EOL;
+}
+echo "</div>";
+
+function generatorDate($days) {
+  for($day = 1; $day < $days; $day++) {
+    yield date("d/m/Y", strtotime("+$day days"));
+  }
+}
+
+echo "<div style='text-align: center'>";
+foreach(generatorDate($iterator) as $date) {
+  echo "<small class='tag'>{$date}</small>";
+}
+echo "</div>";
