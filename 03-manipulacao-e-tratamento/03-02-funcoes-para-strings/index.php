803a250441dad074ed5acc4a2d61cae0d28e956a
Lucas Freire <lucasfreirel.dev@gmail.com>
2024-10-21 22:37:53 -0300

    func string and func arrays

--- 03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
+++ 03-manipulacao-e-tratamento/03-02-funcoes-para-strings/index.php
@@ -6,21 +6,78 @@ fullStackPHPClassName("03.02 - Funções para strings");
  * [ strings e multibyte ] https://php.net/manual/en/ref.mbstring.php
  */
 fullStackPHPClassSession("strings e multibyte", __LINE__);
+$string = "O último show do natirruts foi incrível";
 
+var_dump([
+  "string" => $string,
+  "strlen" => strlen($string),
+  "mb_strlen" => mb_strlen($string),
+  "substr" => substr($string, 9),
+  "mb_substr" => mb_substr($string, 9),
+  "strtoupper" => strtoupper($string),
+  "mb_strtoupper" => mb_strtoupper($string),
+]);
 
 /**
  * [ conversão de caixa ] https://php.net/manual/en/function.mb-convert-case.php
  */
 fullStackPHPClassSession("conversão de caixa", __LINE__);
+$mbString = $string;
 
+var_dump([
+  "mb_strtoupper" => mb_strtoupper($mbString),
+  "mb_strtolower" => mb_strtolower($mbString),
+  "mb_convert_case UPPER" => mb_convert_case($mbString, MB_CASE_UPPER),
+  "mb_convert_case LOWER" => mb_convert_case($mbString, MB_CASE_LOWER),
+  "mb_convert_case TITLE" => mb_convert_case($mbString, MB_CASE_TITLE),
+]);
 
 /**
  * [ substituição ] multibyte and replace
  */
 fullStackPHPClassSession("substituição", __LINE__);
+$mbReplace = $mbString . " Fui, iria novamente, e foi épico!";
 
+var_dump([
+  "mb_strlen" => mb_strlen($mbReplace),
+  "mb_strpos" => mb_strpos($mbReplace, ", "),
+  "mb_strrpos" => mb_strrpos($mbReplace, ", "),
+  "mb_substr" => mb_substr($mbReplace, 40 + 2, 14),
+  "mb_strstr" => mb_strstr($mbReplace, ", ", true),
+  "mb_strrchr" => mb_strrchr($mbReplace, ", ", true),
+]);
+
+$mbStrReplace = $string;
+
+echo "<p>", $mbStrReplace, "</p>";
+echo "<p>", str_replace("natirruts", "bob marley", $mbStrReplace), "</p>";
+echo "<p>", str_replace(["natirruts", "eu fui", "último"], "bob marley", $mbStrReplace), "</p>";
+echo "<p>", str_replace(["natirruts", "incrível"], ["bob marley", "Fod@@"], $mbStrReplace), "</p>";
+
+$article = <<<REGGAE
+  <article>
+    <h3>event</h3>
+    <p>desc</p>
+  </article>
+REGGAE;
+
+$articleData = [
+  "event" => "Luau",
+  "desc" => $mbReplace,
+];
+
+echo str_replace(array_keys($articleData), array_values($articleData), $article);
 
 /**
  * [ parse string ] parse_str | mb_parse_str
  */
 fullStackPHPClassSession("parse string", __LINE__);
+
+$endPoint = "name=Lucas&email=lfs@gmail.com";
+mb_parse_str($endPoint, $parseEndPoint);
+
+var_dump([
+  $endPoint,
+  $parseEndPoint,
+  (object) $parseEndPoint
+]);
--- 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
+++ 03-manipulacao-e-tratamento/03-03-funcoes-para-arrays/index.php
@@ -6,21 +6,115 @@ fullStackPHPClassName("03.03 - Funções para arrays");
  * [ criação ] https://php.net/manual/pt_BR/ref.array.php
  */
 fullStackPHPClassSession("manipulação", __LINE__);
+$index = [
+  "natirruts",
+  "bob marley",
+  "Yg Yg",
+];
+
+$assoc = [
+  "band_1" => "natirruts",
+  "band_2" => "bob marley",
+  "band_3" => "Yg Yg",
+];
+
+array_unshift($index, "", "MVP");
+$assoc = ["band_4" => "MVP", "band_5" => ""] + $assoc;
+
+array_push($index, "");
+$assoc = $assoc + ["band_6" => ""];
+
+array_shift($index);
+array_shift($assoc);
+
+array_pop($index);
+array_pop($assoc);
+
+array_unshift($index, "");
+
+
+var_dump(
+  $index,
+  $assoc
+);
+
+$index = array_filter($index);
+$assoc = array_filter($assoc);
+
+var_dump(
+  $index,
+  $assoc
+);
 
 
 /*
  * [ ordenação ] reverse | asort | ksort | sort
  */
 fullStackPHPClassSession("ordenação", __LINE__);
+$index = array_reverse($index);
+$assoc = array_reverse($assoc);
 
+asort($index);
+asort($assoc);
+
+ksort($index);
+ksort($assoc);
+
+sort($index);
+rsort($assoc);
+
+var_dump(
+  $index,
+  $assoc
+);
 
 /*
  * [ verificação ]  keys | values | in | explode
  */
 fullStackPHPClassSession("verificação", __LINE__);
+var_dump(
+  [
+    array_keys($index),
+    array_values($index),
+  ]
+);
 
+if (in_array("natirruts", $assoc)) {
+  echo "<p>Cause I'm back</p>";
+}
+
+$arrToString = implode(", ", $assoc);
+echo "<p>Eu curto {$arrToString} e outras bandas</p>";
+echo "<p>{$arrToString}</p>";
+
+var_dump(explode(", ", $arrToString));
 
 /**
  * [ exemplo prático ] um template view | implode
  */
 fullStackPHPClassSession("exemplo prático", __LINE__);
+$profile = [
+  "name" => "Lucas",
+  "company" => "LFSolutions",
+  "email" => "lucas@fullstackphp",
+];
+
+$template = <<<TPL
+  <article>
+    <h1>{{name}}</h1>
+    <p>{{company}}</p>
+    <a title="E-mail para {{name}}" href="mailto:{{email}}">Enviar email</a>
+  </article>
+TPL;
+
+echo $template;
+
+echo str_replace(array_keys($profile), array_values($profile), $template);
+
+$replaces = "{{" . implode("}}&{{", array_keys($profile)) . "}}";
+
+echo str_replace(
+  explode("&", $replaces),
+  array_values($profile),
+  $template
+);
